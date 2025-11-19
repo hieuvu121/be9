6a4bab99c9bc082462bc021027ff08e4361f9f3e
hieu <hieubeobilabi@gmail.com>
2025-11-19 11:41:17 +0700

    add files

---
+++ hw11.java
@@ -0,0 +1,44 @@
+package dsa;
+
+import java.util.Arrays;
+
+public class hw11{
+	public static int evenPairs(int[] A) {
+		int count=0;
+		int index=0;
+		while(true) {
+			if(index==A.length-1) {
+				int res=A[index]+A[0];
+				if(res%2==0) {
+					count+=1;
+					return count;
+				}
+				return count;
+			}
+			int res=A[index]+A[index+1];1
+
+			.if(res%2==0) {
+				count+=1;
+				if(index==A.length-2) {return count;}
+				index+=2;
+			}else {index+=1;}
+		}
+	}
+	public static long minNum(int[] A) {
+		Arrays.sort(A);
+		long count=0;
+		for(int i=0;i<A.length;i++) {
+			int val=i+1;
+			if ((i+1)!=A[i]) {
+				count+= Math.abs(A[i]-val);
+			}
+			if(count>1000000000) {return -1;};
+		}
+		return count;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(hw11.evenPairs(new int[] {14,21,16,35,22}));
+		System.out.println(hw11.minNum(new int[] {6,2,3,5,6,3}));
+	}
+}
---
+++ hw12.java
@@ -0,0 +1,75 @@
+package dsa;
+import java.util.*;
+public class hw12 {
+	public static int valueOccur(int[] A) {
+		int count=0;
+		int max=A[A.length-1];
+		int[] arr=new int[max+1];
+		for(int i=0;i<A.length;i++) {
+			int v=A[i];
+			arr[v]++;
+		}
+		for(int i=0;i<arr.length;i++) {
+			if(i!=arr[i]&&arr[i]!=0) {
+				count+=Math.min(arr[i],Math.abs(i-arr[i]));
+			}
+		}
+		return count;
+	}
+	
+	public static int noEqualDigits(int A) {
+		boolean notEqualDigit=true;
+		int digit=A;
+		while(notEqualDigit) {
+			notEqualDigit=false;
+			digit++;
+			String num=String.valueOf(digit);
+			for(int i=0;i<num.length()-1;i++) {
+				if(num.charAt(i)==num.charAt(i+1)) {
+					notEqualDigit=true;
+				}
+				}
+			}
+		return digit;
+		}	
+	
+	public static int sameDigitMerge(int[] A) {
+		HashMap<Integer,Integer> first=new HashMap<>();
+		HashMap<Integer,Integer> last=new HashMap<>();
+		int res=0;
+		for(int i:A) {
+			int lastDigit=i%10;
+			int firstDigit=i;
+			while (firstDigit>=10) {
+				firstDigit /=10;
+			}
+			first.put(firstDigit,first.getOrDefault(firstDigit,0)+1 );
+			last.put(lastDigit, last.getOrDefault(lastDigit,0)+1);
+		}
+		for(Integer key:first.keySet()) {
+			if(last.get(key)!=null){
+				res+=first.get(key)*last.get(key);
+			}
+		}
+		return res;
+	}
+	
+	public static double sqrt(int A) {
+		double x=A;
+		double eps=0.0001;
+		while(true) {
+			double next=0.5*(x+(double) A/x);
+			if(Math.abs(next-x)<0.0001) {
+				return next;
+			}
+			x=next;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(hw12.valueOccur(new int[] {10,10,10}));
+		System.out.println(hw12.noEqualDigits(44432));
+		System.out.println(hw12.sameDigitMerge(new int[] {15,15,15,51,51,51}));
+		System.out.println(hw12.sqrt(26));
+	}
+}
