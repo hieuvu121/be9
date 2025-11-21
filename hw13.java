c48301a2bceb8c8d6b1cde805b61a5f8fc34bb43
hieu <hieubeobilabi@gmail.com>
2025-11-21 20:04:41 +0700

    finalize

--- hw13.java
+++ hw13.java
@@ -1,21 +1,43 @@
 package dsa;
+import java.util.*;
 class hw13{
 	public static int findInterval(int[] A, int r, int l) {
-		int countInterval=r-l+1,res=0;
-		for(int i=0;i<A.length;i++) {
-			if(countInterval==0) {
-				return res;}
-			if(A[i]<=r&&A[i]>=l) {
-				res++;
-				countInterval--;
-			}else {
-				res=0;
-				countInterval=r-l+1;
-			}
+		int leftPointer=0,res=0,missing=r-l+1,rightPointer=0;
+		HashMap<Integer,Integer> checkValid=new HashMap<>();
+		for(int i=l;i<r+1;i++) {
+			checkValid.put(i, 0);
 		}
+
+		for(rightPointer=0;rightPointer<A.length;rightPointer++) {
+			if(A[rightPointer]>=l&&A[rightPointer]<=r) {
+				int oldValue=checkValid.get(A[rightPointer]);
+				if(oldValue==0) {
+					missing--;
+					}
+				checkValid.put(A[rightPointer],oldValue+1);
+			}
+			
+			while(missing==0&&leftPointer<=rightPointer) {
+				if(res==0) {
+					res=rightPointer-leftPointer+1;
+				}else {
+					res=Math.min(res, rightPointer-leftPointer+1);}
+				
+				if(A[leftPointer]>=l&&A[rightPointer]<=r) {
+					int oldValue=checkValid.get(A[leftPointer]);
+					if(oldValue==1) {
+						missing++;
+					}
+					checkValid.put(A[leftPointer], oldValue-1);
+				}
+				leftPointer++;
+				
+				}
+				}
+		if(res!=0) {return res;}
 		return -1;
 	}
 	public static void main(String[] args) {
-		System.out.println(hw13.findInterval(new int[] {1,3,5,7},5,3));
+		System.out.println(hw13.findInterval(new int[] {2, 1, 4, 3, 2, 1, 1, 4},4,2));
 	}
 }
